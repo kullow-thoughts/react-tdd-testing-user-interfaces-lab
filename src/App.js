f36c717bfdd14d6839ee0aa347c67821cfd6c5e8
Abdulhakimkullow <abdulhakim.kullow@student.moringaschool.com>
2024-05-08 07:41:53 +0300

    completed lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm (your name)</h1>
+      <img src="https://media.licdn.com/dms/image/C4D03AQHGTb9U3mwpCQ/profile-displayphoto-shrink_800_800/0/1604401557250?e=1720656000&v=beta&t=jjEyuLMN1dRzwgXDhkZjakgowXaz-PyijQa0HW9JpuM" alt="stephen bowen"/>
+      <h2>About Me</h2>
+      <p>Biography here</p>
+      <a href="https://github.com/bowenSteve">Github</a>
+      <a href="https://www.linkedin.com/in/steven-bowen-65a69a133/">Linkedin</a>
+    </div>
+  );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,76 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+    // Arrange
+    render(<App />);
+    // Act
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /hi, i'm/i,
+        exact: false,
+        level: 1,
+      });
+    // Assert
+    expect(topLevelHeading).toBeInTheDocument();
+  });
+  test("displays an image of yourself with alt text", () => {
+    //Arrange
+    render(<App />)
+    //Act
+    const image = screen.getByAltText(/stephen bowen/i);
+    // Assert
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute("src","https://media.licdn.com/dms/image/C4D03AQHGTb9U3mwpCQ/profile-displayphoto-shrink_800_800/0/1604401557250?e=1720656000&v=beta&t=jjEyuLMN1dRzwgXDhkZjakgowXaz-PyijQa0HW9JpuM")
+  })
+  test("displays a second-level heading with the text 'About Me'", () => {
+    // Arrange
+    render(<App />);
+  
+    // Act
+    const middleLevelHeading = screen.getByRole("heading", {
+      name: /about me/i,
+      exact: false,
+      level: 2,
+    });
+  
+    // Assert
+    expect(middleLevelHeading).toBeInTheDocument();
+  });
+  test("displays a paragraph for your biography", () => {
+    // Arrange
+    render(<App />);
+  
+    // Act
+    const bioParagraph = screen.getByText(/biography/i);
+  
+    // Assert
+    expect(bioParagraph).toBeInTheDocument();
+  });
+  test("displays a link to your GitHub page", () => {
+    // Arrange
+    render(<App />);
+  
+    // Act
+    const githubLink = screen.getByRole("link", {
+      name: /github/i,
+      exact: false,
+    });
+  
+    // Assert
+    expect(githubLink).toBeInTheDocument();
+    expect(githubLink).toHaveAttribute("href", "https://github.com/bowenSteve");
+  });
+  test("displays a link to your Linkedin page", () => {
+    // Arrange
+    render(<App />);
+  
+    // Act
+    const githubLink = screen.getByRole("link", {
+      name: /linkedin/i,
+      exact: false,
+    });
+  
+    // Assert
+    expect(githubLink).toBeInTheDocument();
+    expect(githubLink).toHaveAttribute("href", "https://www.linkedin.com/in/steven-bowen-65a69a133/");
+  });
